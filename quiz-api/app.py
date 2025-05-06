4a91dfbb9a0d3d137a590ef7555dc3fcb54754e1
timotheedumas <timothee.dumas@edu.esiee.fr>
2025-05-06 22:11:19 +0200

    Clean .gitignore + initial API

---
+++ quiz-api/app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from flask_cors import CORS
+
+
+app = Flask(__name__)
+CORS(app)
+
+
+@app.route('/')
+def hello_world():
+    return "Hello, world modif"
+
+if __name__ == "__main__":
+    app.run()
