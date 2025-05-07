e0b4b1a965ee99488576825e090a121418e117dc
timotheedumas <timothee.dumas@edu.esiee.fr>
2025-05-07 02:50:05 +0200

    Implémentation de /quiz-info

--- quiz-api/app.py
+++ quiz-api/app.py
@@ -10,5 +10,9 @@ CORS(app)
 def hello_world():
     return "Hello, world modif"
 
+@app.route('/quiz-info', methods=['GET'])
+def get_quiz_info():
+    return {"size": 0, "scores": []}, 200
+
 if __name__ == "__main__":
     app.run()
